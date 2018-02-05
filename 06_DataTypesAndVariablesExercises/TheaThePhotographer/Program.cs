3dda73ab9c3e9204f03ef6c4f42be23cdb1b78a3
VladiG <vladig1984@gmail.com>
2018-02-05 02:24:40 +0200

    Added C# solution to problem TheaThePhotographer (Data Types and Variables Exercises).

--- 06_DataTypesAndVariablesExercises/TheaThePhotographer/Program.cs
+++ 06_DataTypesAndVariablesExercises/TheaThePhotographer/Program.cs
@@ -7,6 +7,32 @@ namespace TheaThePhotographer
         static void Main(string[] args)
         {
 
+            int numberOfPictures = int.Parse(Console.ReadLine());
+            int filterTimeInSecPerPicture = int.Parse(Console.ReadLine());
+            byte filterFactor = byte.Parse(Console.ReadLine());
+            int uploadTimeInSecPerPicture = int.Parse(Console.ReadLine());
+
+            long filterTimeInSecTotal = (long)numberOfPictures * filterTimeInSecPerPicture;
+            long uploadTimeInSecTotal = (long)Math.Ceiling((double)numberOfPictures * ((double)(filterFactor)
+                    / 100)) * uploadTimeInSecPerPicture;
+            long totalTimeInSec = filterTimeInSecTotal + uploadTimeInSecTotal;
+
+            //-byte seconds = (byte)(((double)totalTimeInSec / 60 - Math.Floor((double)totalTimeInSec / 60)) 
+            //-      * 60);
+            //-byte minutes = (byte)(((double)totalTimeInSec / (60 * 60) - 
+            //-      Math.Floor((double)totalTimeInSec / (60 * 60))) * 60);
+            //-byte hours = (byte)(((double)totalTimeInSec / (24 * 60 * 60) -
+            //-     Math.Floor((double)totalTimeInSec / (24 * 60 * 60))) * 24);
+            //-byte days = (byte)(Math.Floor((double)totalTimeInSec / (24 * 60 * 60)));
+            //Console.WriteLine($"{days}:{hours:D2}:{minutes:D2}:{seconds:D2}");
+
+            TimeSpan time = TimeSpan.FromSeconds(totalTimeInSec);
+            string timeResult = string.Format("{0}:{1:D2}:{2:D2}:{3:D2}",
+                time.Days,
+                time.Hours,
+                time.Minutes,
+                time.Seconds);
+            Console.WriteLine(timeResult);
         }
     }
 }
